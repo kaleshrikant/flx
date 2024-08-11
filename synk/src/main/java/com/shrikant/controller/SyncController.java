bcb3d00a70b5c8ac1152e3dd779c7fd7d8f13201
Shrikant Kale <kaleshrikant@live.com>
2024-08-11 15:05:06 +0530

    updated findAllParticipants()

--- synk/src/main/java/com/shrikant/controller/SyncController.java
+++ synk/src/main/java/com/shrikant/controller/SyncController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @CrossOrigin("*")
 @RestController
@@ -41,9 +43,12 @@ public class SyncController {
     }
 
     @GetMapping("/find")
-    public ResponseEntity<Participant> findAllParticipants() {
-        syncService.findAllParticipants();
-        return new ResponseEntity<>(HttpStatus.FOUND);
+    public ResponseEntity<List<Participant>> findAllParticipants() {
+        List<Participant> allParticipants = syncService.findAllParticipants();
+        if (! allParticipants.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.FOUND).body(allParticipants.stream().collect(Collectors.toUnmodifiableList()));
+        } else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @PutMapping("/update/{participant}")
