663d1e407acd32ef699ced7d5aeb0b6b8241217d
Shrikant Kale <kaleshrikant@live.com>
2024-08-11 14:18:23 +0530

    modified for id = employeeId with Optional

--- synk/src/main/java/com/shrikant/controller/SyncController.java
+++ synk/src/main/java/com/shrikant/controller/SyncController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/sync")
@@ -21,13 +24,20 @@ public class SyncController {
 
 
     @PostMapping("/save")
-    public ResponseEntity<Participant> saveParticipant(Participant participant) {
+    public ResponseEntity<Participant> saveParticipant(@RequestBody Participant participant) {
         return new ResponseEntity<>(syncService.saveParticipant(participant), HttpStatus.CREATED);
     }
 
-    @GetMapping("/find/{participantId}")
-    public ResponseEntity<Participant> findParticipant(@PathVariable Integer participantId) {
-        return new ResponseEntity<>(syncService.findParticipant(participantId), HttpStatus.FOUND);
+    @GetMapping("/find/{employeeId}")
+    public ResponseEntity<Participant> findParticipant(@PathVariable String employeeId) {
+        if (Objects.nonNull(employeeId) && !employeeId.trim().isEmpty()) {
+            Optional<Participant> optionalParticipant = syncService.findParticipant(employeeId);
+            if (optionalParticipant.isPresent()) {
+                return ResponseEntity.status(HttpStatus.FOUND).body(optionalParticipant.get());
+            } else
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @GetMapping("/find")
@@ -37,13 +47,13 @@ public class SyncController {
     }
 
     @PutMapping("/update/{participant}")
-    public ResponseEntity<Participant> updateParticipant(@PathVariable Participant participant) {
+    public ResponseEntity<Participant> updateParticipant(@RequestBody Participant participant) {
         return new ResponseEntity<>(syncService.updateParticipant(participant), HttpStatus.ACCEPTED);
     }
 
-    @DeleteMapping("/delete/{participantId}")
-    public ResponseEntity<Void> deleteParticipant(@PathVariable Integer participantId) {
-        syncService.deleteParticipant(participantId);
+    @DeleteMapping("/delete/{employeeId}")
+    public ResponseEntity<Void> deleteParticipant(@PathVariable String employeeId) {
+        syncService.deleteParticipant(employeeId);
         return new ResponseEntity<>(HttpStatus.GONE);
     }
 
