9ad61471eae4a3008447030e6c056b81b7900cc7
Shrikant Kale <kaleshrikant@live.com>
2024-08-12 12:00:24 +0530

    switched to JpaRepository

--- rcd-bld/src/main/java/com/shrikant/repository/RcbBldRepository.java
+++ rcd-bld/src/main/java/com/shrikant/repository/RcbBldRepository.java
@@ -1,9 +1,9 @@
 package com.shrikant.repository;
 
 import com.shrikant.entity.Participant;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RcbBldRepository extends CrudRepository<Participant, String> {
+public interface RcbBldRepository extends JpaRepository<Participant, String> {
 }
