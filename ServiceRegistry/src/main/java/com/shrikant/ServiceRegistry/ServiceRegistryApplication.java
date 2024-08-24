dea75c5de990d45b803e58304e20a6c7f9fff265
Shrikant Kale <kaleshrikant@live.com>
2024-08-24 13:25:54 +0530

    Initial Commit

---
+++ ServiceRegistry/src/main/java/com/shrikant/ServiceRegistry/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.shrikant.ServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
